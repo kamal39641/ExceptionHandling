62214c4b65de65fb123e3a20b7b1e459882f8063
Kamal <ka627752@gmail.com>
2024-09-15 11:10:19 +0600

    Exception

---
+++ src/BankAccount.java
@@ -0,0 +1,30 @@
+public class BankAccount {
+    private String accountNumber;
+    private double balance;
+
+    public BankAccount(String accountNumber, double initialBalance) {
+        this.accountNumber = accountNumber;
+        this.balance = initialBalance;
+    }
+    public void deposit(double amount) throws InvalidAmountException {
+        if (amount <= 0) {
+            throw new InvalidAmountException("Deposit amount must be positive.");
+        }
+        balance += amount;
+    }
+    public void withdraw(double amount) throws InsufficientFundsException, InvalidAmountException {
+        if (amount <= 0) {
+            throw new InvalidAmountException("Withdrawal amount must be positive.");
+        }
+        if (amount > balance) {
+            throw new InsufficientFundsException("Insufficient funds for this withdrawal.");
+        }
+        balance -= amount;
+    }
+    public double getBalance(){
+        return balance;
+    }
+    public String getAccountNumber(){
+        return accountNumber;
+    }
+}
